236c936e078e12e49c8f1b6cf6269e66a7478ac9
goellnerphillip <phillip@ubuntu01.fritz.box>
2023-08-30 10:55:11 +0200

    Pull the UI building from Karel into a dedicated service

---
+++ src/main/java/com/pgoellner/karel/UiBuilder.java
@@ -0,0 +1,35 @@
+package com.pgoellner.karel;
+
+import javax.swing.*;
+import java.awt.*;
+
+final class UiBuilder {
+    private UiBuilder() {}
+
+    static JPanel createMainPanel() {
+        JPanel mainPanel = new JPanel();
+        BorderLayout layout = new BorderLayout();
+
+        KarelWorld canvas = new KarelWorld();
+        layout.addLayoutComponent(canvas, BorderLayout.CENTER);
+        mainPanel.add(canvas);
+
+        JPanel controls = new JPanel();
+        controls.add(new JButton("Play"));
+        layout.addLayoutComponent(controls, BorderLayout.WEST);
+        mainPanel.add(controls);
+
+        mainPanel.setLayout(layout);
+        return mainPanel;
+    }
+
+    static void createWindow(JPanel content) {
+        JFrame window = new JFrame();
+
+        window.add(content);
+
+        window.setMinimumSize(new Dimension(700, 700));
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setVisible(true);
+    }
+}

52fe6085cdc90f4bd4e9b335786d0a51f63c13ba
goellnerphillip <phillip@ubuntu01.fritz.box>
2023-09-25 12:47:51 +0200

    Implement a way to rotate coordinates

--- src/main/java/com/pgoellner/karel/geometry/CoordinateSystem.java
+++ src/main/java/com/pgoellner/karel/geometry/CoordinateSystem.java
@@ -2,15 +2,14 @@ package com.pgoellner.karel.geometry;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class CoordinateSystem {
-    private final Coordinates origin;
     private final Coordinates furthestCorner;
 
     private final List<Coordinates> allCoordinates;
 
     public CoordinateSystem(Coordinates origin, Coordinates furthestCorner) {
-        this.origin = origin;
         this.furthestCorner = furthestCorner;
 
         this.allCoordinates = new ArrayList<>();
@@ -32,5 +31,54 @@ public class CoordinateSystem {
 
     public List<Coordinates> allCoordinates() {
         return new ArrayList<>(this.allCoordinates);
+    }
+
+    public static List<Coordinates> rotate(Coordinates centreOfRotation, List<Coordinates> pointsToRotate, int degrees) {
+        return pointsToRotate
+                .stream()
+                .map(coordinates -> rotate(centreOfRotation, coordinates, degrees))
+                .collect(Collectors.toList());
+    }
+
+    private static Coordinates rotate(Coordinates centreOfRotation, Coordinates pointToRotate, int degrees) {
+        final Coordinates pointRelativeToOrigin = pointToRotate.minus(centreOfRotation);
+
+        final Coordinates pointRotatedAroundOrigin = new Coordinates(
+                pointRelativeToOrigin.x * cos(degrees) - pointRelativeToOrigin.y * sin(degrees),
+                pointRelativeToOrigin.x * sin(degrees) + pointRelativeToOrigin.y * cos(degrees)
+        );
+
+        return pointRotatedAroundOrigin.plus(centreOfRotation);
+    }
+
+    private static int sin(int degrees) {
+        if (degrees >= 360) {
+            return sin(degrees - 360);
+        } else if (degrees <= -360) {
+            return sin(degrees + 360);
+        }
+
+        switch (degrees) {
+            case 90:
+            case -270: return 1;
+            case -90:
+            case 270: return -1;
+            default: return 0;
+        }
+    }
+
+    private static int cos(int degrees) {
+        if (degrees >= 360) {
+            return cos(degrees - 360);
+        } else if (degrees <= -360) {
+            return cos(degrees + 360);
+        }
+
+        switch (degrees) {
+            case 0: return 1;
+            case -180:
+            case 180: return -1;
+            default: return 0;
+        }
     }
 }
---
+++ src/test/java/com/pgoellner/karel/CoordinateSystemTest.java
@@ -0,0 +1,120 @@
+package com.pgoellner.karel;
+
+import com.pgoellner.karel.geometry.CoordinateSystem;
+import com.pgoellner.karel.geometry.Coordinates;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.List;
+
+class CoordinateSystemTest {
+    @ParameterizedTest
+    @ValueSource(ints = {0, 360, -360})
+    void no_effective_rotation(int degrees) {
+        List<Coordinates> givenCoordinates = ArgumentList.of(
+                new Coordinates(1, 2),
+                new Coordinates(1, 1),
+                new Coordinates(2, 2)
+        );
+
+        List<Coordinates> rotatedCoordinates = CoordinateSystem.rotate(
+                new Coordinates(2, 2),
+                givenCoordinates,
+                degrees
+        );
+
+        List<Coordinates> expectedCoordinates = ArgumentList.of(
+                new Coordinates(1, 2),
+                new Coordinates(1, 1),
+                new Coordinates(2, 2)
+        );
+
+        Assertions.assertEquals(
+                expectedCoordinates,
+                rotatedCoordinates
+        );
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {90, -270})
+    void rotate_effectively_90_degrees(int degrees) {
+        List<Coordinates> givenCoordinates = ArgumentList.of(
+                new Coordinates(1, 2),
+                new Coordinates(1, 1),
+                new Coordinates(2, 2)
+        );
+
+        List<Coordinates> rotatedCoordinates = CoordinateSystem.rotate(
+                new Coordinates(2, 2),
+                givenCoordinates,
+                degrees
+        );
+
+        List<Coordinates> expectedCoordinates = ArgumentList.of(
+                new Coordinates(2, 1),
+                new Coordinates(3, 1),
+                new Coordinates(2, 2)
+        );
+
+        Assertions.assertEquals(
+                expectedCoordinates,
+                rotatedCoordinates
+        );
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {180, -180})
+    void rotate_effectively_180_degrees(int degrees) {
+        List<Coordinates> givenCoordinates = ArgumentList.of(
+                new Coordinates(1, 2),
+                new Coordinates(1, 1),
+                new Coordinates(2, 2)
+        );
+
+        List<Coordinates> rotatedCoordinates = CoordinateSystem.rotate(
+                new Coordinates(2, 2),
+                givenCoordinates,
+                degrees
+        );
+
+        List<Coordinates> expectedCoordinates = ArgumentList.of(
+                new Coordinates(3, 2),
+                new Coordinates(3, 3),
+                new Coordinates(2, 2)
+        );
+
+        Assertions.assertEquals(
+                expectedCoordinates,
+                rotatedCoordinates
+        );
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {270, -90})
+    void rotate_270_degrees(int degrees) {
+        List<Coordinates> givenCoordinates = ArgumentList.of(
+                new Coordinates(1, 2),
+                new Coordinates(1, 1),
+                new Coordinates(2, 2)
+        );
+
+        List<Coordinates> rotatedCoordinates = CoordinateSystem.rotate(
+                new Coordinates(2, 2),
+                givenCoordinates,
+                degrees
+        );
+
+        List<Coordinates> expectedCoordinates = ArgumentList.of(
+                new Coordinates(2, 3),
+                new Coordinates(1, 3),
+                new Coordinates(2, 2)
+        );
+
+        Assertions.assertEquals(
+                expectedCoordinates,
+                rotatedCoordinates
+        );
+    }
+}

f4f9f8b88dc6dc2985dcb125c130e422b8ec503d
goellnerphillip <phillip@ubuntu01.fritz.box>
2024-10-17 14:13:02 +0200

    Add more ArchUnit tests

--- karel/src/test/java/com/pgoellner/karel/architecture/Karel.java
+++ karel/src/test/java/com/pgoellner/karel/architecture/Karel.java
@@ -3,6 +3,7 @@ package com.pgoellner.karel.architecture;
 import com.tngtech.archunit.core.domain.JavaClasses;
 import com.tngtech.archunit.core.importer.ClassFileImporter;
 import com.tngtech.archunit.lang.ArchRule;
+import com.tngtech.archunit.library.freeze.FreezingArchRule;
 import org.junit.jupiter.api.Test;
 
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.*;
@@ -12,8 +13,11 @@ class Karel {
 
     @Test
     void should_be_decoupled() {
-        ArchRule rule = noClasses()
-                .should().dependOnClassesThat().resideInAPackage("..localization..");
+        ArchRule rule = FreezingArchRule.freeze(noClasses()
+                .should().dependOnClassesThat().resideInAnyPackage(
+                        "..localization..",
+                        "..parse.."
+                ));
 
         rule.check(karel);
     }
